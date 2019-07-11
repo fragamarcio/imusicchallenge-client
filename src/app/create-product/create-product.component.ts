b24f586a7b2bb208034a0885a04afe16201dc096
Marcio Fraga <ffragamarcio@gmail.com>
2019-07-11 02:47:18 -0300

    Criada edição de produtos.

--- src/app/create-product/create-product.component.ts
+++ src/app/create-product/create-product.component.ts
@@ -1,6 +1,8 @@
 import { ProductService } from './../product.service';
 import { Product } from './../product';
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-create-product',
@@ -11,21 +13,48 @@ export class CreateProductComponent implements OnInit {
 
   product: Product =  new Product();
   submited = false;
+  isNew = true;
+  id = null;
 
-  constructor(private productService : ProductService) { }
+  constructor(private productService : ProductService, private route: ActivatedRoute) {
+   
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    
+    if(this.route.snapshot.url[0].path == 'edit'){
+      this.id = this.route.snapshot.params.id;
+      this.isNew = false;
+      this.load(this.id);
+    } else {
+      this.id = null;
+      this.isNew == false;
+    }
+  }
+    
   newProduct(): void {
     this.submited = false;
     this.product = new Product();
   }
 
+  load(id : String) {
+    this.productService.getProduct(id).subscribe(
+      data =>{
+        this.product = data.data;
+      },
+      error => console.log(error));
+  }
+
   save() {
-    this.productService.createProduct(this.product)
-    .subscribe(data => console.log(data), error => console.log(error));
-    this.product = new Product();
+    if(this.isNew){
+      this.productService.createProduct(this.product)
+      .subscribe(data => console.log(data), error => console.log(error));
+      this.product = new Product();
+    } else {
+      this.productService.updateProduct(this.id, this.product)
+      .subscribe(data => console.log(data), error => console.log(error));
+    }
   }
 
   onSubmit(){
